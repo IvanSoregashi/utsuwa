4701e92c38be400f35c14d1192c2ae7e1ec7cf8f
IvanSoregashi <ivanshiriaev2222@gmail.com>
2026-06-27 15:08:48 +0500

    downloads, webdav

--- helpers/apps/samba.sh
+++ helpers/apps/samba.sh
@@ -12,14 +12,18 @@ echo "--> Initializing Samba Server configuration & shares..."
 
 # Create data shared folders
 mkdir -p "${SECURE_PATH}/vault"
+mkdir -p "${SECURE_PATH}/webdav"
 mkdir -p "${SECURE_PATH}/apps"
 mkdir -p "${DATA_PATH}/gallery"
 mkdir -p "${DATA_PATH}/books"
+mkdir -p "${DATA_PATH}/downloads"
 
 # Align permissions
 chown -R "${SYS_USER}:${SYS_USER}" "${SECURE_PATH}/vault"
+chown -R "${SYS_USER}:${SYS_USER}" "${SECURE_PATH}/webdav"
 chown -R "${SYS_USER}:${SYS_USER}" "${SECURE_PATH}/apps"
 chown -R "${SYS_USER}:${SYS_USER}" "${DATA_PATH}/gallery"
 chown -R "${SYS_USER}:${SYS_USER}" "${DATA_PATH}/books"
+chown -R "${SYS_USER}:${SYS_USER}" "${DATA_PATH}/downloads"
 
 echo "  Samba shares initialized successfully."
